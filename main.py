74c6920a5e83e69852754915e59d27f54c2bdb93
windowcow <65646883+windowcow@users.noreply.github.com>
2022-11-29 11:49:43 +0900

    MIN good

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tabulate as tb
 import pandas as pd
 
+EMPTY = '-'
 
 def get_file_contents(input_file_name):
     """ 파일의 이름을 받아서 각 줄을 list로 반환하는 함수 """
@@ -15,7 +16,7 @@ def get_NMWK(file_contents):
     return map(int, file_contents[0].split("  "))
 
 def get_page_reference_string(file_contents):
-    return list(map(int, file_contents[1].split(" ")))
+    return file_contents[1].replace(" ", "")
 
 def print_inputs(N, M, W, K, page_reference_string):
     print("N: " + str(N))
@@ -27,44 +28,44 @@ def print_inputs(N, M, W, K, page_reference_string):
     return 0
 
 def print_table(time, page_reference_string, memory_state, page_fault):
-    table = [
-        ["Time", *time],
-        ["Ref. string", *page_reference_string], 
-        ["Memory state", *memory_state],
-        ["Page fault", *page_fault]
-        ]
+    table = [["Time", *time],
+            ["Ref. string", *page_reference_string], 
+            ["Memory state", *memory_state],
+            ["Page fault", *page_fault]]
+    
     print(tb.tabulate(table, tablefmt="fancy_grid"))
 
 def get_index_of_victim_in_current_memory_state(current_memory_state, page_reference_string, time):
-    
-    pass
+    # 참고로 여기의 current_memory_state에는 EMPTY가 없다.
+    reference_time = [page_reference_string.find(current_memory_state[i], time + 1) for i in range(len(current_memory_state))]
+    result_index = reference_time.index(max(reference_time))
+    if -1 not in reference_time:
+        result_index = reference_time.index(max(reference_time))
+    else:
+        result_index = reference_time.index(-1)
+    return result_index
 
 def get_result(M: int, page_reference_string: str):
     def make_empty_memory_state(M: int):
-        return [-1 for _ in range(M)]
+        return [EMPTY for _ in range(M)]
     
     
     current_memory_state = make_empty_memory_state(M)
     
     for time in range(len(page_reference_string)):
         current_requested_page_frame = page_reference_string[time]
+        print(time, ":", current_memory_state)
         
         if current_requested_page_frame in current_memory_state: # page fault가 아닌 경우
             continue
         else: # page fault인 경우
-            victim_page_frame_index = get_index_of_victim_in_current_memory_state(current_memory_state, page_reference_string, time)
-
-
-git branch -M main
-git remote add origin https://github.com/windowcow/2022-2-OS-project3.git
-git push -u origin main
-
-
-
-
-
-
-
+            
+            if EMPTY in current_memory_state:
+                current_memory_state[current_memory_state.index(EMPTY)] = current_requested_page_frame
+            else:
+                victim_page_frame_index = get_index_of_victim_in_current_memory_state(current_memory_state, page_reference_string, time)
+                current_memory_state[victim_page_frame_index] = current_requested_page_frame
+                print("victim_page_frame_index: ", victim_page_frame_index)
 
 
 if __name__ == "__main__":
@@ -80,5 +81,6 @@ if __name__ == "__main__":
     memory_state = [0 for i in range(len(page_reference_string))]
     page_fault = [0 for i in range(len(page_reference_string))]
     print_table(time, page_reference_string, memory_state, page_fault)
+    get_result(M, page_reference_string)
 
 
